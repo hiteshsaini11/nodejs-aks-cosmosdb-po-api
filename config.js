dc6e271c029ba473da565baa7aea15c28b5e1476
hiteshsaini11 <saini.hitesh@hotmail.com>
2018-09-24 10:50:13 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 let config = {}
 
 config.host = process.env.HOST || "https://vcs-cosmosdb.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "1pxILrCIRPhunE5p9R6tA4Y5ZrqJGyNiCKD3LBh3zUSAADMuoaMa0AtdHVujR8ysWDAGTsyRtwqibLoSlnwt0A==
-";
+config.authKey = process.env.AUTH_KEY || "1pxILrCIRPhunE5p9R6tA4Y5ZrqJGyNiCKD3LBh3zUSAADMuoaMa0AtdHVujR8ysWDAGTsyRtwqibLoSlnwt0A==";
 config.databaseId = "OrderDB";
 config.collectionId = "PurchaseOrders";
 
